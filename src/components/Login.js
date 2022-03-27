82b1f23dabfc35d04dd2922d997826fee5d4eee5
avishith <avishithpm@gmail.com>
2022-03-28 00:49:05 +0530

    edited the banner

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,7 +22,7 @@ const Login = ()=>{
   return (
     <div id="login-page">
         <div id="login-card">
-            <h2>Welcome to Unichat</h2>
+            <h2>Welcome to FastChat</h2>
             <div 
                 className="login-button google"
                 onClick={loginWithGoogle}
